c8da6f5d52017cd55c12491d1ae87d676cf36611
iwilliams83 <indirajamurath@gmail.com>
2018-06-10 18:08:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,61 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sum1 = @side1 + @side2
+    sum2 = @side2 + @side3
+    sum3 = @side3 + @side1
+    if @side1 == @side2 && @side2 == @side3
+        if @side1 == 0 || @side2 == 0 || @side3 == 0
+          begin
+            raise TriangleError
+          #rescue TriangleError => error
+          #puts error.message
+        end
+      else
+        return :equilateral
+      end
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      if sum1 <= @side3 || sum2 <= @side1 || sum3 <= @side2
+        begin
+          raise TriangleError
+        #rescue TriangleError => error
+          #puts error.message
+        end
+      elsif @side1 == 0 || @side2 == 0 || @side3 == 0
+        begin
+          raise TriangleError
+        end
+      else
+        return :isosceles
+      end
+    elsif @side1 != @side2 && @side2 != @side3
+      if @side1 == 0 || @side2 == 0 || @side3 == 0
+        begin
+          raise TriangleError
+          rescue TriangleError => error
+          puts error.message
+        end
+      elsif sum1 < @side3 || sum2 < @side1 || sum3 < @side2
+        begin
+          raise TriangleError
+        end
+      else
+        return :scalene
+      end
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+    def message
+      "This is an invalid triangle."
+    end
 end
